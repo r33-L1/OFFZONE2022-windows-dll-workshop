ed755408ef77a2588fd16c5d78d61d7f4b7a0286
r33-L1 <31613623+r33-L1@users.noreply.github.com>
2022-08-25 12:22:06 +0300

    Create ProxyLib.cpp

---
+++ ProxyLib/ProxyLib.cpp
@@ -0,0 +1,48 @@
+#include <Windows.h>
+#include <iostream>
+#include "intercepts.h"
+
+typedef int(__stdcall* f_print_hello)();
+
+BOOL APIENTRY DllMain(HMODULE hModule,
+	DWORD  ul_reason_for_call,
+	LPVOID lpReserved
+)
+{
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+		::MessageBoxW(NULL, L"HackProxy DllMain", L"HackProxyLib", 0);
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+	return TRUE;
+}
+
+extern "C" int print_hello() {
+	HINSTANCE			hSimpleLib;
+
+	hSimpleLib = LoadLibraryA("SimpleLib_1.dll");
+	if (hSimpleLib == NULL) {
+		std::cout << "Error: Couldn't load Target DLL" << std::endl;
+		return -1;
+	}
+
+	f_print_hello intrcpt_print_hello = (f_print_hello)GetProcAddress(hSimpleLib, "print_hello");
+	if (!intrcpt_print_hello) {
+		std::cout << "Cannot locate the function print_hello" << std::endl;
+		return -1;
+	}
+
+	// any code
+	::MessageBoxW(NULL, L"print_hello_intercepted", L"HackProxyLib", 0);
+	
+
+	// Call original print_hello can be done here
+	intrcpt_print_hello();
+
+	return 0;
+
+}
