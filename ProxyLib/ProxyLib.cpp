0d80f51b67370ca6d21fe5f77b8b72cb7d9ebcbc
r33-L1 <31613623+r33-L1@users.noreply.github.com>
2022-08-25 14:04:22 +0300

    Update ProxyLib.cpp

--- ProxyLib/ProxyLib.cpp
+++ ProxyLib/ProxyLib.cpp
@@ -12,7 +12,7 @@ BOOL APIENTRY DllMain(HMODULE hModule,
 	switch (ul_reason_for_call)
 	{
 	case DLL_PROCESS_ATTACH:
-		::MessageBoxW(NULL, L"HackProxy DllMain", L"HackProxyLib", 0);
+		::MessageBoxW(NULL, L"ProxyLib DllMain", L"ProxyLib", 0);
 	case DLL_THREAD_ATTACH:
 	case DLL_THREAD_DETACH:
 	case DLL_PROCESS_DETACH:
@@ -37,7 +37,7 @@ extern "C" int print_hello() {
 	}
 
 	// any code
-	::MessageBoxW(NULL, L"print_hello_intercepted", L"HackProxyLib", 0);
+	::MessageBoxW(NULL, L"print_hello_intercepted", L"ProxyLib", 0);
 	
 
 	// Call original print_hello can be done here
